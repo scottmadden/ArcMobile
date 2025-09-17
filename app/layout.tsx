94cd173a13450bf76e029f218639c53e05452506
scottmadden <49500605+scottmadden@users.noreply.github.com>
2025-09-16 20:43:02 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 // app/layout.tsx
-import './styles/globals.css'; // ensure Tailwind is included
+import '../styles/globals.css'; // ensure Tailwind is included
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
