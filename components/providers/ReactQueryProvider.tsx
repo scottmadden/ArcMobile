e8778541dd67dc73a54b3f60a393bb25576366be
scottmadden <49500605+scottmadden@users.noreply.github.com>
2025-09-17 06:43:09 -0400

    Create ReactQueryProvider.tsx

---
+++ components/providers/ReactQueryProvider.tsx
@@ -0,0 +1,8 @@
+'use client';
+import * as React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  const [client] = React.useState(() => new QueryClient());
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
